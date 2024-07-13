fc3717eaffd280efbfe16b59eaa762156f06c097
Akash Ch  Moulik <159507156+AkashChMoulik@users.noreply.github.com>
2024-07-13 11:28:56 +0530

    Update EM.m

--- EM.m
+++ EM.m
@@ -1,14 +1,13 @@
-
 % Parameters
 mu = 1.0;
 sigma = 0.1;
-T = 10; % Total time
+T = 1; % Total time changed to 1
 num_steps = 10^6; % Number of time steps
 
 % Time step sizes for each method
-dt_EulerMaruyama = 1e-6;
-dt_Milstein = 1e-4;
-dt_Taylor15 = 1e-2;
+dt_EulerMaruyama = T / num_steps;
+dt_Milstein = T / num_steps;
+dt_Taylor15 = T / num_steps;
 
 % Initial conditions
 X0 = 1.0;
@@ -44,33 +43,35 @@ for i = 1:num_steps
     % Milstein method
     X_Milstein(i+1) = X_Milstein(i) + Y_Milstein(i) * dt_Milstein;
     Y_Milstein(i+1) = Y_Milstein(i) + (mu * (1 - X_Milstein(i)^2) * Y_Milstein(i) - X_Milstein(i)) * dt_Milstein + ...
-                      0.5 * sigma * (Y_Milstein(i) * (2 * X_Milstein(i) * dW_Milstein(i) + sigma * dW_Milstein(i)^2) ...
-                      + (mu * (1 - X_Milstein(i)^2) - 1) * dt_Milstein);
+                      sigma * dW_Milstein(i) + 0.5 * sigma^2 * dW_Milstein(i) * (dW_Milstein(i) - dt_Milstein);
     
     % Taylor 1.5 method
     X_Taylor15(i+1) = X_Taylor15(i) + Y_Taylor15(i) * dt_Taylor15;
     Y_Taylor15(i+1) = Y_Taylor15(i) + (mu * (1 - X_Taylor15(i)^2) * Y_Taylor15(i) - X_Taylor15(i)) * dt_Taylor15 + ...
-                      0.5 * sigma * (Y_Taylor15(i) * (2 * X_Taylor15(i) * dW_Taylor15(i) + sigma * dW_Taylor15(i)^2) ...
-                      + (mu * (1 - X_Taylor15(i)^2) - 1) * dt_Taylor15);
+                      sigma * dW_Taylor15(i) + 0.5 * sigma^2 * dW_Taylor15(i) * (dW_Taylor15(i) - dt_Taylor15);
 end
 
 % Plot results
+time = linspace(0, T, num_steps+1);
+
 figure;
 subplot(2, 1, 1);
-plot(linspace(0, T, num_steps+1), X_EM, 'b', 'LineWidth', 1.5);
+plot(time, X_EM, 'b', 'LineWidth', 2);
 hold on;
-plot(linspace(0, T, num_steps+1), X_Milstein, 'r', 'LineWidth', 1.5);
-plot(linspace(0, T, num_steps+1), X_Taylor15, 'g', 'LineWidth', 1.5);
+plot(time, X_Milstein, 'r', 'LineWidth', 2);
+plot(time, X_Taylor15, 'g', 'LineWidth', 2);
+xlim([0 1]);
 xlabel('Time');
 ylabel('X(t)');
 title('Comparison of X(t) using Different Methods');
 legend('Euler-Maruyama', 'Milstein', 'Taylor 1.5');
 
 subplot(2, 1, 2);
-plot(linspace(0, T, num_steps+1), Y_EM, 'b', 'LineWidth', 1.5);
+plot(time, Y_EM, 'b', 'LineWidth', 2);
 hold on;
-plot(linspace(0, T, num_steps+1), Y_Milstein, 'r', 'LineWidth', 1.5);
-plot(linspace(0, T, num_steps+1), Y_Taylor15, 'g', 'LineWidth', 1.5);
+plot(time, Y_Milstein, 'r', 'LineWidth', 2);
+plot(time, Y_Taylor15, 'g', 'LineWidth', 2);
+xlim([0 1]);
 xlabel('Time');
 ylabel('Y(t)');
 title('Comparison of Y(t) using Different Methods');
